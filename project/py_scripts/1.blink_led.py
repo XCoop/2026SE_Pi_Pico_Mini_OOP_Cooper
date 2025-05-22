27e412b4e9b05bee2b7f4d3245616135e38919aa
XCoop <cooper.lucas3@education.nsw.gov.au>
2025-05-22 10:39:00 +1000

    mcra

---
+++ project/py_scripts/1.blink_led.py
@@ -0,0 +1,17 @@
+from machine import Pin
+from time import sleep
+
+# Wait for USB to become ready
+sleep(0.1)
+
+#store desired output pin in a variable
+led_pin = 25
+
+#configure GPIO Pin as an output pin and create and led object for Pin class
+led = Pin(led_pin, Pin.OUT)
+
+while True:
+    led.value(True)  #turn on the LED
+    sleep(1)   #wait for one second
+    led.value(False)  #turn off the LED
+    sleep(1)   #wait for one second
