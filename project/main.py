77776d20358122fdf64ed58e0e399da1207c8002
XCoop <cooper.lucas3@education.nsw.gov.au>
2025-05-28 15:06:20 +1000

    commit

--- project/main.py
+++ project/main.py
@@ -7,7 +7,7 @@ from time import sleep
 sleep(0.1)
 
 # File name of the script to import
-file_name = "v04"
+file_name = "3.analog_sensor"
 
 # Add the path to the sys.path
 sys.path.append("/py_scripts")
